818d2a46ad0e6c4537e29bb56601e5e20a14e980
gnatroshvili <gnatroshvili@credo.ge>
2023-12-10 00:52:41 +0400

    finish

---
+++ README.md
@@ -0,0 +1,1 @@
+Kazbegi Website
---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+function onLoad() {
+  const ham = document.querySelector(".ham-menu");
+  const nav = document.querySelector(".navbar");
+  ham.addEventListener("click", () => {
+    ham.classList.toggle("active");
+    nav.classList.toggle("active");
+  });
+
+  const loadder = document.getElementById("preloadder");
+  loadder.style.display = "none";
+}
+
+window.addEventListener("load", () => {
+  onLoad();
+});
