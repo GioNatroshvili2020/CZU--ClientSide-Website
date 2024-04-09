866d782b7d2c42cec6ef412bb47a44a6b2a957d2
GioNatroshvili2020 <gnatroshvili2099@sdsu.edu>
2024-04-10 00:15:51 +0400

    debugging

--- signin_process.php
+++ signin_process.php
@@ -1,6 +1,6 @@
 <!-- signin_process.php -->
 <?php
-session_start();
+//session_start();
 
 // Include database connection
 include_once 'config.php';
@@ -21,27 +21,27 @@ $sql = "SELECT * FROM users WHERE email = '$email'";
 $result = $connection->query($sql);
 
 echo 'aq moxvedi?'; 
-if (mysqli_num_rows($result) == 1) {
-    // User found
-    echo 'aqane var';
-    $user = mysqli_fetch_assoc($result);
-    if (password_verify($password, $user['password_hash'])) {
-        // Password is correct, start session and set user data
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_name'] = $user['name'];
-        $_SESSION['logged_in'] = true;
+// if (mysqli_num_rows($result) == 1) {
+//     // User found
+//     echo 'aqane var';
+//     $user = mysqli_fetch_assoc($result);
+//     if (password_verify($password, $user['password_hash'])) {
+//         // Password is correct, start session and set user data
+//         $_SESSION['user_id'] = $user['id'];
+//         $_SESSION['user_name'] = $user['name'];
+//         $_SESSION['logged_in'] = true;
 
-        // Redirect to dashboard or any other page
-        header('Location: index.html');
-        exit();
-    } else {
-        // Password is incorrect
-        echo "Invalid email or password";
-    }
-} else {
-    // User not found
-    echo "Invalid email or password";
-}
+//         // Redirect to dashboard or any other page
+//         header('Location: index.html');
+//         exit();
+//     } else {
+//         // Password is incorrect
+//         echo "Invalid email or password";
+//     }
+// } else {
+//     // User not found
+//     echo "Invalid email or password";
+// }
 
 // Close connection
 mysqli_close($connection);
