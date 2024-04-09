b8f3c29712608147e4ad202d64d3830619f80573
GioNatroshvili2020 <gnatroshvili2099@sdsu.edu>
2024-04-09 17:39:40 +0400

    captcha styles2

--- sign_up.php
+++ sign_up.php
@@ -95,7 +95,7 @@
                         <label for="captcha">Captcha</label>
                         <input type="text" name="captcha" id="captcha" placeholder="Enter the captcha" required />
                         <!-- Display the captcha image -->
-                        <img style="  width: 120px; height: 60px;" src="captcha.php" alt="Captcha Image" />
+                        <img style="  width: 120px; height: 45px; margin-top:20px;" src="captcha.php" alt="Captcha Image" />
                     </div>
                     <button class="btn-send">Sign Up</button>
                 </form>
